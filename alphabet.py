e91906580e8d9b16b91b499ba8b1db3eb52b700f
roganoff <matematic1337@gmail.com>
2022-10-24 22:11:51 +0300

    solved

--- alphabet.py
+++ alphabet.py
@@ -1,10 +1,12 @@
 num = int(input("Enter a num of value: "))
-arr_ternary_num = list()
+ternary_num = list()
 alphabet = {0: "A", 1: "O", 2: "Y"}
 
 rem = num - 1
 while rem > 0:
-    arr_ternary_num.append(rem % 3)
+    ternary_num.append(rem % 3)
     rem //= 3
-    
-print(arr_ternary_num)
+
+for i in range(0, len(ternary_num)):
+    ternary_num[i] = alphabet[ternary_num[i]]
+print(''.join(reversed(ternary_num)))
