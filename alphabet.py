5fc00776d5506df7dea28c8d3e8e00519ed18ae1
roganoff <matematic1337@gmail.com>
2022-10-24 20:57:36 +0300

    govno

--- alphabet.py
+++ alphabet.py
@@ -1,3 +1,10 @@
 num = int(input("Enter a num of value: "))
-ternary_num = 0
-arr_ternary_num = {}
+arr_ternary_num = list()
+alphabet = {0: "A", 1: "O", 2: "Y"}
+
+rem = num - 1
+while rem > 0:
+    arr_ternary_num.append(rem % 3)
+    rem //= 3
+    
+print(arr_ternary_num)
